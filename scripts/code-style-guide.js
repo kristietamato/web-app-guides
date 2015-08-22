0bf7733d0aa387ed934049ac7ff22bb1adbbc584
Kristie <kristietamato@gmail.com>
2015-08-21 21:34:14 -0700

    Add open/close onClick for table of contents

--- scripts/code-style-guide.js
+++ scripts/code-style-guide.js
@@ -1,29 +1,39 @@
-var buttonGeneral = document.getElementById('button-general');
+function openClose(div)
+{
+  if (div.style.display !== 'none')
+  {
+     div.style.display = 'none';
+  }
+  else
+  {
+     div.style.display = 'block';
+  }
+}
 
+var buttonGeneral = document.getElementById('button-general');
 buttonGeneral.onclick = function()
 {
-  var div = document.getElementById('open-close-general');
-  if (div.style.display !== 'none')
-  {
-     div.style.display = 'none';
-  }
-  else
-  {
-     div.style.display = 'block';
-  }
+  var divGeneral = document.getElementById('open-close-general');
+  openClose(divGeneral);
 };
 
-var buttonCoder = document.getElementById('button-coder');
-
-buttonCoder.onclick = function()
+var buttonHTML = document.getElementById('button-html');
+buttonHTML.onclick = function()
 {
-  var div = document.getElementById('open-close-coder');
-  if (div.style.display !== 'none')
-  {
-     div.style.display = 'none';
-  }
-  else
-  {
-     div.style.display = 'block';
-  }
+  var divHTML = document.getElementById('open-close-html');
+  openClose(divHTML);
+};
+
+var buttonCSS = document.getElementById('button-css');
+buttonCSS.onclick = function()
+{
+  var divCSS = document.getElementById('open-close-css');
+  openClose(divCSS);
+};
+
+var buttonJS = document.getElementById('button-js');
+buttonJS.onclick = function()
+{
+  var divJS = document.getElementById('open-close-js');
+  openClose(divJS);
 };
